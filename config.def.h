130aa8476b14cdc13b67213cbeab3520c8be0517
AzuraAce <oliverjjst@proton.me>
2025-09-24 18:56:34 +0200

    applied background image

--- config.def.h
+++ config.def.h
@@ -15,3 +15,6 @@ ResourcePref resources[] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* Background image path, should be available to the user above */
+static const char* background_image = "";
