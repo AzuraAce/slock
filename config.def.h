c30a6640f27d40b2a9e03d90416c4400aaaca284
AzuraAce <oliverjjst@proton.me>
2025-09-24 19:14:49 +0200

    applied dwmlogo ontop of background image

--- config.def.h
+++ config.def.h
@@ -1,20 +1,41 @@
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[BACKGROUND] =   "black",     /* after initialization */
+	[INIT] =   "#2d2d2d",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
+
+/* Background image path, should be available to the user above */
+static char* background_image = "";
 
 /*
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "color0",       STRING,  &colorname[INIT] },
-		{ "color4",       STRING,  &colorname[INPUT] },
-		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "colinit",       STRING,  &colorname[INIT] },
+		{ "colinput",       STRING,  &colorname[INPUT] },
+		{ "colfailed",       STRING,  &colorname[FAILED] },
+        { "bgimage",      STRING,  &background_image },
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
-/* Background image path, should be available to the user above */
-static const char* background_image = "";
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+
+};
